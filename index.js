ed6ebca40bbd3b4c3e75683ddfc7f2431915d3de
Dhrubot <dhrubo.dh@gmail.com>
2020-07-21 04:04:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    arr = []
+    for (let i = 0; i < array.length; i++) {
+        arr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return arr;
+}
+
+function countDown(int) {
+
+    let i = int;
+
+    while (i >= 0 ) {
+        console.log(i);
+        i --;
+    }
+}
